dfcbdd3c6adecc326fd7235ba4011e4595dc6cc4
Sudheer <sudheerkumarsaladi4203@gmail.com>
2024-11-17 02:48:17 +0530

    third update

--- src/test/java/selenium/io.java
+++ src/test/java/selenium/io.java
@@ -1,1 +1,17 @@
-//sample code
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class io {
+	
+	@Test
+	public void test() throws InterruptedException
+	{
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.youtube.com/");
+		Thread.sleep(2000);
+	}
+
+}
