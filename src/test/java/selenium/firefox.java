c134af4a7eafe08409fbf6d18722e1d02678ea54
Sudheer <sudheerkumarsaladi4203@gmail.com>
2024-11-17 02:40:00 +0530

    today update

---
+++ src/test/java/selenium/firefox.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class firefox {
+	
+	@Test
+	public void test() throws InterruptedException
+	{
+
+		WebDriver driver = new EdgeDriver();
+		driver.get("https:/google.com/");
+		Thread.sleep(2000);
+	}
+
+}
